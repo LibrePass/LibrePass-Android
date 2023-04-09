8313d1d4b9940915a2baabbec74845bf2e8ca5fb
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-04-09 22:26:53 +0200

    Fix ksp warning

--- app/src/main/java/dev/medzik/librepass/android/data/CredentialsDatabase.kt
+++ app/src/main/java/dev/medzik/librepass/android/data/CredentialsDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Credentials::class], version = 1)
+@Database(entities = [Credentials::class], version = 1, exportSchema = false)
 abstract class CredentialsDatabase : RoomDatabase() {
     abstract fun credentialsDao(): CredentialsDao
 }

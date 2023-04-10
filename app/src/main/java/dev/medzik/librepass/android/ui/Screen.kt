8d0c09062ff5a319a6fee17ef39cd5e9e4db5ef5
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-04-10 12:03:21 +0200

    Use enum class for screens in compose navigation

---
+++ app/src/main/java/dev/medzik/librepass/android/ui/Screen.kt
@@ -0,0 +1,13 @@
+package dev.medzik.librepass.android.ui
+
+enum class Screen {
+    Login,
+    Dashboard
+}
+
+//class Screen {
+//    companion object {
+//        const val Login = "login"
+//        const val Dashboard = "dashboard"
+//    }
+//}

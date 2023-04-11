a7d86dd0438bab07ed7d31ac411c98c802aca909
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-04-11 16:34:21 +0200

    Add Welcome and Register screens

--- app/src/main/java/dev/medzik/librepass/android/ui/composable/TopBar.kt
+++ app/src/main/java/dev/medzik/librepass/android/ui/composable/TopBar.kt
@@ -1,5 +1,6 @@
 package dev.medzik.librepass.android.ui.composable
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -15,6 +16,28 @@ fun TopBar(title: String) {
                 text = title,
                 style = MaterialTheme.typography.titleLarge,
             )
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBarTwoColor(text1: String, text2: String) {
+    TopAppBar(
+        title = {
+            Row {
+                Text(
+                    text = text1,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+
+                Text(
+                    text = text2,
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+            }
         }
     )
 }

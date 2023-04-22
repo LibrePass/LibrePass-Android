3a2bc2e6cf033e6fde1e91b4aca66e86ac5f7960
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-04-22 21:34:47 +0200

    Cleanup code

--- app/src/main/java/dev/medzik/librepass/android/ui/Screen.kt
+++ app/src/main/java/dev/medzik/librepass/android/ui/Screen.kt
@@ -1,25 +1,5 @@
 package dev.medzik.librepass.android.ui
 
-//enum class Screen(data: String? = null) {
-//    Welcome,
-//    Register,
-//    Login,
-//    Unlock,
-//    Dashboard("{encryptionKey}"),
-//    CipherViewScreen("{cipherId}?encryptionKey={encryptionKey}"),
-//    CipherAdd("{encryptionKey}"),
-//
-//    ; // <- semicolon is required
-//
-//    val get = if (data != null) "${name}/${data}" else name
-//
-//    companion object {
-//        fun Dashboard(encryptionKey: String) = Dashboard.get.replace("{encryptionKey}", encryptionKey)
-//        fun CipherViewScreen(cipherId: String, encryptionKey: String) = CipherViewScreen.get.replace("{cipherId}", cipherId).replace("{encryptionKey}", encryptionKey)
-//        fun CipherAdd(encryptionKey: String) = CipherViewScreen.get.replace("{encryptionKey}", encryptionKey)
-//    }
-//}
-
 enum class Argument {
     EncryptionKey,
     CipherId,
@@ -43,7 +23,6 @@ sealed class Screen(private val route: String, private val arguments: List<Strin
 
     @Throws(IllegalArgumentException::class)
     fun fill(vararg arguments: Pair<Argument, String>): String {
-
         if (arguments.size != this.arguments?.size) {
             println("Invalid number of arguments. Expected ${this.arguments?.size}, got ${arguments.size}")
             throw IllegalArgumentException("Invalid number of arguments. Expected ${this.arguments?.size}, got ${arguments.size}")

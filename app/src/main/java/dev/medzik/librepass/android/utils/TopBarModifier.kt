402fe44f69fc57a2d523c89a84a9b719691f1949
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-04-23 14:26:34 +0200

    delete TopBarModifier because added innerPadding

--- app/src/main/java/dev/medzik/librepass/android/utils/TopBarModifier.kt
+++
@@ -1,7 +0,0 @@
-package dev.medzik.librepass.android.utils
-
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-
-val TopBarModifier = Modifier.padding(top = 60.dp) // 56 + 4 (on 56 text field is cut off)

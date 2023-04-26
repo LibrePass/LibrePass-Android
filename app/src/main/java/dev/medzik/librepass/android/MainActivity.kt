11a78453f46b582d407ae03264f905227868b7c2
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-04-26 21:49:11 +0200

    Add support for editing ciphers

--- app/src/main/java/dev/medzik/librepass/android/MainActivity.kt
+++ app/src/main/java/dev/medzik/librepass/android/MainActivity.kt
@@ -13,8 +13,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dev.medzik.librepass.android.data.Repository
+import dev.medzik.librepass.android.ui.Argument
 import dev.medzik.librepass.android.ui.Screen
-import dev.medzik.librepass.android.ui.screens.CipherAddView
+import dev.medzik.librepass.android.ui.screens.CipherAddEditView
 import dev.medzik.librepass.android.ui.screens.CipherViewScreen
 import dev.medzik.librepass.android.ui.screens.DashboardScreen
 import dev.medzik.librepass.android.ui.screens.LoginScreen
@@ -22,6 +23,7 @@ import dev.medzik.librepass.android.ui.screens.RegisterScreen
 import dev.medzik.librepass.android.ui.screens.UnlockScreen
 import dev.medzik.librepass.android.ui.screens.WelcomeScreen
 import dev.medzik.librepass.android.ui.theme.LibrePassTheme
+import java.util.UUID
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -72,7 +74,21 @@ fun LibrePassApp() {
         }
 
         composable(Screen.CipherAdd.get) {
-            CipherAddView(navController = navController)
+            CipherAddEditView(navController = navController)
+        }
+
+        composable(Screen.CipherEdit.get) {
+            val cipherId = navController.previousBackStackEntry?.arguments?.getString(Argument.CipherId.get)
+                ?: return@composable
+
+            val encryptionKey = navController.previousBackStackEntry?.arguments?.getString(Argument.EncryptionKey.get)
+                ?: return@composable
+
+            val cipherTable = repository.cipher.get(UUID.fromString(cipherId)) ?: return@composable
+
+            val cipher = cipherTable.encryptedCipher.toCipher(encryptionKey)
+
+            CipherAddEditView(navController = navController, baseCipher = cipher)
         }
     }
 }

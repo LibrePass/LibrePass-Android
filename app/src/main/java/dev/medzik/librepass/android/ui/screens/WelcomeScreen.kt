981a34c4b210f7ce6bf252a720f54dc6938a1377
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-04-30 21:19:59 +0200

    Fix deprecation code in code that getting application icon

--- app/src/main/java/dev/medzik/librepass/android/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/dev/medzik/librepass/android/ui/screens/WelcomeScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.google.accompanist.drawablepainter.DrawablePainter
@@ -23,10 +24,10 @@ import dev.medzik.librepass.android.ui.composable.common.TopBarTwoColor
 
 @Composable
 fun WelcomeScreen(navController: NavController) {
-    // TODO: fix deprecated code
-    val applicationInfo = navController.context.packageManager.getApplicationInfo(navController.context.packageName, 0)
-    val packageManager = navController.context.packageManager
-    val icon: Drawable = applicationInfo.loadIcon(packageManager)
+    // get app icon
+    val icon: Drawable = LocalContext.current.packageManager.getApplicationIcon(
+        LocalContext.current.packageName
+    )
 
     Scaffold(
         topBar = {

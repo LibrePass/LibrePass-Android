85e2b1f6104166f8afaa763c593abf4f4e994c85
M3DZIK <me@medzik.dev>
2023-05-03 15:17:51 +0200

    ci(nightly): setup app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.medzik.librepass.android"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = System.getenv("LIBREPASS_APP_VERSION_CODE")?.toInt() ?: 1
+        versionName = System.getenv("LIBREPASS_APP_VERSION_NAME") ?: "0.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

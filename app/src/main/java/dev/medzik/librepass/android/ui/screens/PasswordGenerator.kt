92a10c6aeacc4221dce262d77f3db27833adce94
M3DZIK <me@medzik.dev>
2023-05-06 23:05:13 +0200

    Add configuration options in Password Generator

--- app/src/main/java/dev/medzik/librepass/android/ui/screens/PasswordGenerator.kt
+++ app/src/main/java/dev/medzik/librepass/android/ui/screens/PasswordGenerator.kt
@@ -2,26 +2,33 @@ package dev.medzik.librepass.android.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ContentCopy
 import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import dev.medzik.librepass.android.R
@@ -29,11 +36,62 @@ import dev.medzik.librepass.android.ui.composables.common.TopBar
 import dev.medzik.librepass.android.ui.composables.common.TopBarBackIcon
 import java.util.Random
 
+enum class PasswordType(val literals: String) {
+    NUMERIC("1234567890"),
+    LOWERCASE("abcdefghijklmnopqrstuvwxyz"),
+    UPPERCASE("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+    SYMBOLS("!@#\$%^&*()_+-=[]{}\\|;:'\",.<>/?")
+}
+
 @Composable
 fun PasswordGenerator(navController: NavController) {
     var password by remember { mutableStateOf("") }
 
+    // TODO: save to preferences
+    var passwordLength by remember { mutableStateOf(10L) }
+    var withCapitalLetters by remember { mutableStateOf(true) }
+    var withNumbers by remember { mutableStateOf(true) }
+    var withSymbols by remember { mutableStateOf(true) }
+
     val clipboardManager = LocalClipboardManager.current
+
+    @Composable
+    fun TypeSwitcher(text: String, checked: Boolean, onCheckedChange: (Boolean) -> Unit) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = text,
+                modifier = Modifier.weight(1f)
+            )
+
+            Switch(
+                checked = checked,
+                onCheckedChange = onCheckedChange
+            )
+        }
+    }
+
+    fun generatePassword(): String {
+        var letters = PasswordType.LOWERCASE.literals
+
+        if (withCapitalLetters) {
+            letters += PasswordType.UPPERCASE.literals
+        }
+
+        if (withNumbers) {
+            letters += PasswordType.NUMERIC.literals
+        }
+
+        if (withSymbols) {
+            letters += PasswordType.SYMBOLS.literals
+        }
+
+        return (1..passwordLength)
+            .map { Random().nextInt(letters.length) }
+            .map(letters::get)
+            .joinToString("")
+    }
 
     Scaffold(
         topBar = {
@@ -50,7 +108,6 @@ fun PasswordGenerator(navController: NavController) {
                 .padding(innerPadding)
                 .padding(horizontal = 16.dp)
         ) {
-            // Show password text with, next to it, a button to copy it to clipboard with fixed width
             Row {
                 OutlinedTextField(
                     value = password,
@@ -67,26 +124,7 @@ fun PasswordGenerator(navController: NavController) {
                             }
 
                             IconButton(
-                                onClick = {
-                                    val leftLimit = 48 // numeral '0'
-
-                                    val rightLimit = 122 // letter 'z'
-
-                                    val size = 10L
-
-                                    val random = Random()
-
-                                    password =
-                                        random.ints(leftLimit, rightLimit + 1)
-                                            .filter { i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97) }
-                                            .limit(size)
-                                            .collect(
-                                                { StringBuilder() },
-                                                java.lang.StringBuilder::appendCodePoint,
-                                                java.lang.StringBuilder::append
-                                            )
-                                            .toString()
-                                }
+                                onClick = { password = generatePassword() }
                             ) {
                                 Icon(
                                     imageVector = Icons.Default.Refresh,
@@ -99,6 +137,58 @@ fun PasswordGenerator(navController: NavController) {
                     modifier = Modifier.weight(1f)
                 )
             }
+
+            Spacer(modifier = Modifier.padding(top = 8.dp))
+
+            // Password length options
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                OutlinedTextField(
+                    value = passwordLength.toString(),
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(end = 8.dp),
+                    label = { Text(text = stringResource(id = R.string.length)) },
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                    onValueChange = { if (it.length in 1..256) passwordLength = it.toLong() }
+                )
+
+                // - and + buttons
+                IconButton(onClick = { if (passwordLength > 1) passwordLength-- }) {
+                    Icon(
+                        imageVector = Icons.Default.Remove,
+                        contentDescription = stringResource(id = R.string.decrease)
+                    )
+                }
+                IconButton(onClick = { passwordLength++ }) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = stringResource(id = R.string.increase)
+                    )
+                }
+            }
+
+            // Capital letters switch
+            TypeSwitcher(
+                text = stringResource(id = R.string.capital_letters),
+                checked = withCapitalLetters,
+                onCheckedChange = { withCapitalLetters = it }
+            )
+
+            // Numeric switch
+            TypeSwitcher(
+                text = stringResource(id = R.string.numbers),
+                checked = withNumbers,
+                onCheckedChange = { withNumbers = it }
+            )
+
+            // Symbols switch
+            TypeSwitcher(
+                text = stringResource(id = R.string.symbols),
+                checked = withSymbols,
+                onCheckedChange = { withSymbols = it }
+            )
 
             // submit button
             Button(

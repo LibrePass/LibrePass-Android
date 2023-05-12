86b63275ae77c293713a851a542fffb473cbc70f
M3DZIK <me@medzik.dev>
2023-05-12 21:24:39 +0200

    Add biometric authentication
    You need to clear the application data if you use the application before this commit

--- app/src/main/java/dev/medzik/librepass/android/MainActivity.kt
+++ app/src/main/java/dev/medzik/librepass/android/MainActivity.kt
@@ -1,16 +1,16 @@
 package dev.medzik.librepass.android
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.fragment.app.FragmentActivity
 import dev.medzik.librepass.android.ui.LibrePassNavController
 import dev.medzik.librepass.android.ui.theme.LibrePassTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 

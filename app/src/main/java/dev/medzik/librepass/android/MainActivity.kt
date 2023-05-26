ac3e2749083d990d17184bf7b9bb22ec1fb04f42
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-05-26 21:53:10 +0200

    Handle uncaught exceptions

--- app/src/main/java/dev/medzik/librepass/android/MainActivity.kt
+++ app/src/main/java/dev/medzik/librepass/android/MainActivity.kt
@@ -1,6 +1,7 @@
 package dev.medzik.librepass.android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -14,6 +15,11 @@ import dev.medzik.librepass.android.ui.theme.LibrePassTheme
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Handle uncaught exceptions
+        Thread.setDefaultUncaughtExceptionHandler { _, e ->
+            Log.e("LibrePass", "Uncaught exception", e)
+        }
 
         // This will lay out our app behind the system bars
         WindowCompat.setDecorFitsSystemWindows(window, false)

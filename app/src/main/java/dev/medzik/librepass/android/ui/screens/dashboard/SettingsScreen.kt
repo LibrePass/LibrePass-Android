f661cd1eb4e7cf9717a8d81c7611df4bc026812e
M3DZIK <me@medzik.dev>
2023-05-28 12:02:44 +0200

    Update Settings screen

--- app/src/main/java/dev/medzik/librepass/android/ui/screens/dashboard/SettingsScreen.kt
+++ app/src/main/java/dev/medzik/librepass/android/ui/screens/dashboard/SettingsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ColorLens
 import androidx.compose.material.icons.filled.Fingerprint
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Switch
@@ -25,6 +26,7 @@ import androidx.navigation.NavController
 import dev.medzik.librepass.android.R
 import dev.medzik.librepass.android.data.Repository
 import dev.medzik.librepass.android.ui.Argument
+import dev.medzik.librepass.android.ui.composables.Group
 import dev.medzik.librepass.android.utils.KeyStoreAlias
 import dev.medzik.librepass.android.utils.KeyStoreUtils
 import dev.medzik.librepass.android.utils.navigation.getString
@@ -44,6 +46,7 @@ fun SettingsScreen(navController: NavController) {
     val credentials = repository.credentials.get()!!
 
     var biometricEnabled by remember { mutableStateOf(credentials.biometricEnabled) }
+    var dynamicColor by remember { mutableStateOf(credentials.dynamicColor) }
 
     // coroutine scope
     val scope = rememberCoroutineScope()
@@ -119,14 +122,45 @@ fun SettingsScreen(navController: NavController) {
         )
     }
 
-    LazyColumn(modifier = Modifier.padding(horizontal = 24.dp)) {
+    LazyColumn(modifier = Modifier.padding(horizontal = 16.dp)) {
         item {
-            TypeSwitcher(
-                icon = Icons.Default.Fingerprint,
-                text = stringResource(id = R.string.Settings_BiometricUnlock),
-                checked = biometricEnabled,
-                onCheckedChange = { biometricChecked() }
-            )
+            Group(
+                name = stringResource(id = R.string.Settings_Group_Appearance)
+            ) {
+                // TODO: add theme selection
+
+                TypeSwitcher(
+                    icon = Icons.Default.ColorLens,
+                    text = stringResource(id = R.string.Settings_MaterialYou),
+                    checked = dynamicColor,
+                    onCheckedChange = {
+                        dynamicColor = it
+                        scope.launch {
+                            repository.credentials.update(
+                                credentials.copy(
+                                    dynamicColor = it
+                                )
+                            )
+                        }
+
+                        // restart activity
+                        context.recreate()
+                    }
+                )
+            }
+        }
+
+        item {
+            Group(
+                name = stringResource(id = R.string.Settings_Group_Security)
+            ) {
+                TypeSwitcher(
+                    icon = Icons.Default.Fingerprint,
+                    text = stringResource(id = R.string.Settings_BiometricUnlock),
+                    checked = biometricEnabled,
+                    onCheckedChange = { biometricChecked() }
+                )
+            }
         }
     }
 }

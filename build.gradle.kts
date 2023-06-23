4ac04b14ad36e4de65db5f0eafd1dafcb765cfdd
M3DZIK <me@medzik.dev>
2023-06-23 11:30:40 +0200

    Remove ktlint from gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,15 +8,9 @@ plugins {
     alias(libs.plugins.com.android.library) apply false
     alias(libs.plugins.com.google.devtools.ksp) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
-
-    // ktlint
-    alias(libs.plugins.org.jlleitschuh.gradle.ktlint) apply false
 }
 
 allprojects {
-    // apply ktlint
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
-
     // fix task (current target is 1.8) and 'kspDebugKotlin' task (current target is 17) jvm target compatibility should be set to the same Java version.
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {

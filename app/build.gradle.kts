7027fe444aaa497416860ffafc449d67a9e5a89d
M3DZIK <me@medzik.dev>
2023-06-27 23:23:08 +0200

    Compile and target app to Android SDK 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "dev.medzik.librepass.android"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "dev.medzik.librepass.android"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = System.getenv("LIBREPASS_APP_VERSION_CODE")?.toInt() ?: 1
         versionName = System.getenv("LIBREPASS_APP_VERSION_NAME") ?: "0.0.0"
 

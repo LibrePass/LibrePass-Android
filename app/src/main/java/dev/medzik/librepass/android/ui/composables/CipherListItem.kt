f1e4b71312561afc79c593ab85060c8ec1b295a6
M3DZIK <me@medzik.dev>
2023-07-07 15:28:13 +0200

    Load icons for ciphers from website

--- app/src/main/java/dev/medzik/librepass/android/ui/composables/CipherListItem.kt
+++ app/src/main/java/dev/medzik/librepass/android/ui/composables/CipherListItem.kt
@@ -1,15 +1,21 @@
 package dev.medzik.librepass.android.ui.composables
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.MoreHoriz
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -25,6 +31,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import dev.medzik.librepass.android.R
 import dev.medzik.librepass.types.cipher.Cipher
 import dev.medzik.librepass.types.cipher.CipherType
@@ -74,7 +82,17 @@ fun CipherListItem(
                 .padding(vertical = 8.dp, horizontal = 24.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Icon(Icons.Default.AccountCircle, contentDescription = null)
+            val domain = (
+                if ((cipher.loginData?.uris?.size ?: 0) > 0)
+                    cipher.loginData!!.uris!![0]
+                else
+                    "https://librepass.medzik.dev"
+                )
+            AsyncImage(
+                model = "https://librepass-api.medzik.dev/api/cipher/icon?domain=$domain",
+                contentDescription = null,
+                modifier = Modifier.size(24.dp)
+            )
 
             Column(
                 modifier = Modifier
@@ -114,6 +132,44 @@ fun CipherListItem(
                 Icon(Icons.Default.MoreHoriz, contentDescription = null)
             }
         }
+    }
+}
+
+@Composable
+fun imageFromURL() {
+    // on below line we are creating a column,
+    Column(
+        // in this column we are adding modifier
+        // to fill max size, mz height and max width
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxHeight()
+            .fillMaxWidth()
+            // on below line we are adding
+            // padding from all sides.
+            .padding(10.dp),
+        // on below line we are adding vertical
+        // and horizontal arrangement.
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // on below line we are adding image for our image view.
+        Image(
+            // on below line we are adding the image url
+            // from which we will  be loading our image.
+            painter = rememberAsyncImagePainter("https://media.geeksforgeeks.org/wp-content/uploads/20210101144014/gfglogo.png"),
+
+            // on below line we are adding content
+            // description for our image.
+            contentDescription = "gfg image",
+
+            // on below line we are adding modifier for our
+            // image as wrap content for height and width.
+            modifier = Modifier
+                .wrapContentSize()
+                .wrapContentHeight()
+                .wrapContentWidth()
+        )
     }
 }
 

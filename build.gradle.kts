3826822e24e9fa32c006cafbf24c530a28d02ece
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-07-08 12:32:55 +0200

    Refactor gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-// https://youtrack.jetbrains.com/issue/KTIJ-19369/False-positive-cant-be-called-in-this-context-by-implicit-receiver-with-plugins-in-Gradle-version-catalogs-as-a-TOML-file#focus=Comments-27-6204464.0-0
-@Suppress("DSL_SCOPE_VIOLATION")
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     // trick: for the same plugin versions in all sub-modules

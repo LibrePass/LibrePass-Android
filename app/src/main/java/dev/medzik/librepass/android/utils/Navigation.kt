e865a104a9023046bc947adcfce9f84eeb8c3f56
M3DZIK <me@medzik.dev>
2023-07-18 14:22:12 +0200

    Refactor code
    Signed-off-by: M3DZIK <me@medzik.dev>

--- app/src/main/java/dev/medzik/librepass/android/utils/Navigation.kt
+++ app/src/main/java/dev/medzik/librepass/android/utils/Navigation.kt
@@ -10,31 +10,7 @@ object Navigation {
      * Get string argument from current screen in [NavController].
      */
     fun NavController.getString(argument: Argument): String? {
-        return currentBackStackEntry?.arguments?.getString(argument.get)
-    }
-
-    /**
-     * Navigate to [Screen] with given [arguments].
-     * @param screen [Screen] to navigate to.
-     * @param arguments Pair of [Argument] and [String].
-     * @param disableBack [Boolean] to disable back navigation.
-     */
-    fun NavController.navigate(
-        screen: Screen,
-        arguments: List<Pair<Argument, String>>,
-        disableBack: Boolean = false
-    ) {
-        val args = arguments.toTypedArray()
-        navigate(
-            route = screen.fill(*args),
-            builder = {
-                if (disableBack) {
-                    popUpTo(graph.startDestinationId) {
-                        inclusive = true
-                    }
-                }
-            }
-        )
+        return currentBackStackEntry?.arguments?.getString(argument.name)
     }
 
     /**

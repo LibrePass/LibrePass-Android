ded143b024eb6afb2e566eff18b310ea40c85b85
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-08-04 23:07:37 +0200

    Bump librepass client
    Signed-off-by: M3DZIK <me@medzik.dev>

--- app/src/main/java/dev/medzik/librepass/android/utils/exception/ApiException.kt
+++ app/src/main/java/dev/medzik/librepass/android/utils/exception/ApiException.kt
@@ -6,15 +6,19 @@ import dev.medzik.librepass.client.errors.ApiException
 import dev.medzik.librepass.responses.ResponseError
 
 fun ApiException.getTranslatedErrorMessage(context: Context): String {
-    return when (getResponseError()) {
+    return when (responseError) {
         ResponseError.INVALID_CREDENTIALS ->
             context.getString(R.string.API_Error_INVALID_CREDENTIALS)
+
         ResponseError.RE_LOGIN_REQUIRED ->
             context.getString(R.string.API_Error_RE_LOGIN_REQUIRED)
+
         ResponseError.EMAIL_NOT_VERIFIED ->
             context.getString(R.string.API_Error_EMAIL_NOT_VERIFIED)
+
         ResponseError.TOO_MANY_REQUESTS ->
             context.getString(R.string.API_Error_TOO_MANY_REQUESTS)
+
         ResponseError.DATABASE_DUPLICATED_KEY ->
             context.getString(R.string.API_Error_DATABASE_DUPLICATED_KEY)
         ResponseError.UNEXPECTED_SERVER_ERROR ->

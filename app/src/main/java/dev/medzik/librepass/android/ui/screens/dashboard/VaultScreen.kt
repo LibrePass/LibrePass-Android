e1181de207562e7bcbcd0beb230361ef7d85ada6
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-10-14 23:03:16 +0200

    Add spacer on the floating action button to completely display the last entry

--- app/src/main/java/dev/medzik/librepass/android/ui/screens/dashboard/VaultScreen.kt
+++ app/src/main/java/dev/medzik/librepass/android/ui/screens/dashboard/VaultScreen.kt
@@ -1,6 +1,8 @@
 package dev.medzik.librepass.android.ui.screens.dashboard
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -11,6 +13,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
@@ -188,6 +191,12 @@ fun VaultScreen(navController: NavController) {
                             }
                         }
                     }
+                )
+            }
+
+            item {
+                Spacer(
+                    modifier = Modifier.size(72.dp)
                 )
             }
         }

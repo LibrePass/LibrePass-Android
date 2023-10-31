4c5eb120748466306fee46bacc20753bec2c6288
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-10-31 21:17:12 +0100

    ci: fix release

--- .github/scripts/get-changelog.sh
+++ .github/scripts/get-changelog.sh
@@ -3,4 +3,4 @@
 VERSION_CODE=$(sed -n 's/.*versionCode\s*=\s*\([0-9]\+\).*/\1/p' app/build.gradle.kts)
 CHANGELOG=$(cat fastlane/metadata/android/en-US/changelogs/$VERSION_CODE.txt)
 
-echo -n "$CHANGELOG"
+echo "$CHANGELOG"
---
+++ t
@@ -0,0 +1,5 @@
+CHANGELOG<<EOF
+- Fixed padding for keyboard
+- Only allows using biometric when it is available
+- Allows biometric authentication to be enabled immediately after login
+EOF

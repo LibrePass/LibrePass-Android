4f16d2690fdf9c1ebade30802a7ab7627862d26e
Oskar Karpiński <karpinski.oskar@outlook.com>
2023-12-31 14:14:23 +0100

    Add Material3 PullRefresh

---
+++ m3-pullrefresh/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "androidx.compose.material3.pullrefresh"
+    compileSdk = libs.versions.android.sdk.compile.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.sdk.min.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = false
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidx.compose.compiler.get()
+    }
+}
+
+dependencies {
+    compileOnly(libs.androidx.material3)
+    compileOnly(libs.androidx.ui)
+}

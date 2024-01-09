1e8349eb7f2b02871d19bdd01d1b7a63d72aa53e
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-01-09 22:04:31 +0100

    Fix kotlin warning
    Replaced `Enum.values()` by `Enum.entries`
    
    ---
    'Enum.values()' is recommended to be replaced by 'Enum.entries' since 1.9

--- app/src/main/java/dev/medzik/librepass/android/utils/ShortenName.kt
+++ app/src/main/java/dev/medzik/librepass/android/utils/ShortenName.kt
@@ -4,8 +4,9 @@ const val SHORTEN_NAME_LENGTH = 16
 const val SHORTEN_USERNAME_LENGTH = 20
 
 /**
- * Returns the string shortened to the given length.
- * @param length the length of characters to shorten
- * @return the string shortened
+ * Returns a string shortened to the specified length.
+ *
+ * @param length Length of characters to which it will be shortened.
+ * @return The shortened string.
  */
 fun String.shorten(length: Int) = if (this.length > length) take(length) + "..." else this

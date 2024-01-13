d0d009fed0a3fd7810f2483b758e4127f820bc2f
M3DZIK <me@medzik.dev>
2024-01-13 16:30:52 +0100

    Refactor code

--- components/src/main/java/dev/medzik/android/components/Dialog.kt
+++ components/src/main/java/dev/medzik/android/components/Dialog.kt
@@ -21,28 +21,28 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+/** Remembers the state of dialog controller visibility. */
 @Composable
 fun rememberDialogState() = remember { DialogState() }
 
-/** A visibility controller for a dialog */
+/** Visibility controller for dialogs. */
 class DialogState {
     var isVisible by mutableStateOf(false)
 
-    /** Show the dialog. */
     fun show() {
         isVisible = true
     }
 
-    /** Hide the dialog. */
     fun hide() {
         isVisible = false
     }
 }
 
 /**
- * A composable function for displaying a basic dialog.
- * @param state the state that controls visibility of the dialog
- * @param content the content of the dialog to display
+ * Basic dialog composable.
+ *
+ * @param state The state that controls visibility of the dialog.
+ * @param content The content of the dialog to display.
  */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -97,31 +97,34 @@ fun BaseDialogPreview() {
 }
 
 /**
- * A composable function for displaying a picker dialog with a list of items.
- * @param state the state that controls visibility of the dialog
- * @param title the title to display at the top of the dialog
- * @param items the list of items to display in the picker
- * @param onSelected a callback function invoked when the item is selected
- * @param content composable lambda that defines the visual representation of each item in the picker
+ * Picker dialog with a list of items.
+ *
+ * @param state The state that controls visibility of the dialog.
+ * @param title The title to display at the top of the dialog.
+ * @param items The list of items to display in the picker.
+ * @param onSelected A callback function invoked when the item is selected.
+ * @param content Composable lambda that defines the visual representation of each item in the picker.
  */
 @Composable
 fun <T> PickerDialog(
     state: DialogState,
-    title: String,
+    title: String?,
     items: List<T>,
     onSelected: (T) -> Unit,
     content: @Composable (T) -> Unit
 ) {
     BaseDialog(state) {
         Column {
-            Text(
-                text = title,
-                fontWeight = FontWeight.Black,
-                modifier =
-                    Modifier
-                        .padding(horizontal = 24.dp)
-                        .padding(bottom = 8.dp)
-            )
+            if (title != null) {
+                Text(
+                    text = title,
+                    fontWeight = FontWeight.Black,
+                    modifier =
+                        Modifier
+                            .padding(horizontal = 24.dp)
+                            .padding(bottom = 8.dp)
+                )
+            }
 
             items.forEach { item ->
                 Box(
--- utils/src/main/java/dev/medzik/android/utils/Toast.kt
+++ utils/src/main/java/dev/medzik/android/utils/Toast.kt
@@ -1,18 +1,22 @@
-@file:Suppress("UNUSED")
-
 package dev.medzik.android.utils
 
 import android.content.Context
 import android.widget.Toast
 import androidx.annotation.StringRes
 
-/** Shows the toast dialog. */
+/**
+ * Show toast dialog.
+ * @param text The text to show.
+ */
 fun Context.showToast(text: String) =
     runOnUiThread {
         Toast.makeText(this, text, Toast.LENGTH_LONG).show()
     }
 
-/** Shows the toast dialog. */
+/**
+ * Show toast dialog.
+ * @param resId The resource id of the string resource to use.
+ */
 fun Context.showToast(
     @StringRes resId: Int
 ) = runOnUiThread {

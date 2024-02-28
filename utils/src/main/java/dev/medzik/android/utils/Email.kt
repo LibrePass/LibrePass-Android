f2cd08a26fd51aac9c15bf3bb77dfda881fbd2ad
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-02-28 21:54:06 +0100

    Format code with ktlint

--- utils/src/main/java/dev/medzik/android/utils/Email.kt
+++ utils/src/main/java/dev/medzik/android/utils/Email.kt
@@ -4,10 +4,16 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 
-fun Activity.openEmailApplication(email: String, subject: String, body: String) {
-    val intent = Intent(
-        Intent.ACTION_SENDTO,
-        Uri.parse("mailto:${email}"))
+fun Activity.openEmailApplication(
+    email: String,
+    subject: String,
+    body: String
+) {
+    val intent =
+        Intent(
+            Intent.ACTION_SENDTO,
+            Uri.parse("mailto:$email")
+        )
     intent.putExtra(Intent.EXTRA_SUBJECT, subject)
     intent.putExtra(Intent.EXTRA_TEXT, body)
     startActivity(Intent.createChooser(intent, "Email"))

5accb9bf2c7ee9ec964985f94fada1ed18fb1ab9
M3DZIK <me@medzik.dev>
2024-04-08 22:40:04 +0200

    Release v1.3.0
    - Updated translations.
    - Bumped dependencies.
    - Changed icon for password generator.
    - Better navigation animations.
    - Fixed network error toast by ignoring network errors.
    - Fixed copying otp code, copy it to clipboard without space.
    - Moved OTP field position, now it is under password field.
    - Optimized API and changed to the new sync API to minimize network usage.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.medzik.librepass.android"
         minSdk = libs.versions.android.sdk.min.get().toInt()
         targetSdk = libs.versions.android.sdk.target.get().toInt()
-        versionCode = 16
-        versionName = "1.2.4"
+        versionCode = 17
+        versionName = "1.3.0"
 
         vectorDrawables {
             useSupportLibrary = true

39d429e8d4f82604a0a796b55c3ab7bf91ba992d
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-05-25 15:28:31 +0200

    Format code

--- app/src/main/java/dev/medzik/librepass/android/utils/Biometric.kt
+++ app/src/main/java/dev/medzik/librepass/android/utils/Biometric.kt
@@ -30,13 +30,12 @@ fun showBiometricPromptForSetup(
     onAuthenticationSucceeded: (Cipher) -> Unit,
     onAuthenticationFailed: () -> Unit
 ) {
-    val promptInfo =
-        BiometricPrompt.PromptInfo.Builder()
-            .setTitle(context.getString(R.string.BiometricSetup_Title))
-            .setSubtitle(context.getString(R.string.BiometricSetup_Subtitle))
-            .setNegativeButtonText(context.getString(R.string.BiometricSetup_Button_Cancel))
-            .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG)
-            .build()
+    val promptInfo = BiometricPrompt.PromptInfo.Builder()
+        .setTitle(context.getString(R.string.BiometricSetup_Title))
+        .setSubtitle(context.getString(R.string.BiometricSetup_Subtitle))
+        .setNegativeButtonText(context.getString(R.string.BiometricSetup_Button_Cancel))
+        .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG)
+        .build()
 
     showBiometricPrompt(context, promptInfo, cipher, onAuthenticationSucceeded, onAuthenticationFailed)
 }
@@ -48,29 +47,28 @@ private fun showBiometricPrompt(
     onAuthenticationSucceeded: (Cipher) -> Unit,
     onAuthenticationFailed: () -> Unit
 ) {
-    val biometricPrompt =
-        BiometricPrompt(
-            context,
-            object : BiometricPrompt.AuthenticationCallback() {
-                override fun onAuthenticationError(
-                    errorCode: Int,
-                    errString: CharSequence
-                ) = onAuthenticationFailed()
+    val biometricPrompt = BiometricPrompt(
+        context,
+        object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(
+                errorCode: Int,
+                errString: CharSequence
+            ) = onAuthenticationFailed()
 
-                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) =
-                    onAuthenticationSucceeded(result.cryptoObject?.cipher!!)
+            override fun onAuthenticationSucceeded(
+                result: BiometricPrompt.AuthenticationResult
+            ) = onAuthenticationSucceeded(result.cryptoObject?.cipher!!)
 
-                override fun onAuthenticationFailed() = onAuthenticationFailed()
-            }
-        )
+            override fun onAuthenticationFailed() = onAuthenticationFailed()
+        }
+    )
 
     biometricPrompt.authenticate(promptInfo, BiometricPrompt.CryptoObject(cipher))
 }
 
 fun checkIfBiometricAvailable(context: Context): Boolean {
-    val status =
-        BiometricManager.from(context)
-            .canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG)
+    val status = BiometricManager.from(context)
+        .canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG)
 
     // return true when available
     return status == BiometricManager.BIOMETRIC_SUCCESS

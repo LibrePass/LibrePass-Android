f9f6012eccefd2c4a992233b1a48005546f21f35
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-05-25 15:13:15 +0200

    Move server choice to a component to prevent duplicated code

---
+++ app/src/main/java/dev/medzik/librepass/android/ui/components/auth/ChoiceServer.kt
@@ -0,0 +1,94 @@
+package dev.medzik.librepass.android.ui.components.auth
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import dev.medzik.android.components.PickerDialog
+import dev.medzik.android.components.navigate
+import dev.medzik.android.components.rememberDialogState
+import dev.medzik.librepass.android.BuildConfig
+import dev.medzik.librepass.android.R
+import dev.medzik.librepass.android.ui.Screen
+import dev.medzik.librepass.android.utils.SecretStore.readKey
+import dev.medzik.librepass.android.utils.StoreKey
+import dev.medzik.librepass.client.Server
+
+@Composable
+fun ChoiceServer(navController: NavController, server: MutableState<String>) {
+    val serverChoiceDialog = rememberDialogState()
+
+    @Composable
+    fun getServerName(server: String): String {
+        return when (server) {
+            Server.PRODUCTION -> {
+                stringResource(R.string.Server_Official)
+            }
+
+            Server.TEST -> {
+                stringResource(R.string.Server_Testing)
+            }
+
+            else -> server
+        }
+    }
+
+    Row(
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            .clickable { serverChoiceDialog.show() }
+    ) {
+        Text(
+            text = stringResource(R.string.ServerAddress) + ": ",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+        )
+
+        Text(
+            text = getServerName(server.value),
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+    
+    var servers = listOf(Server.PRODUCTION)
+        .plus(LocalContext.current.readKey(StoreKey.CustomServers))
+        .plus("custom_server")
+
+    if (BuildConfig.DEBUG) servers = servers.plus(Server.TEST)
+
+    PickerDialog(
+        state = serverChoiceDialog,
+        title = stringResource(R.string.ServerAddress),
+        items = servers,
+        onSelected = {
+            if (it == "custom_server") {
+                navController.navigate(Screen.AddCustomServer)
+            } else {
+                server.value = it
+            }
+        }
+    ) {
+        Text(
+            text = when (it) {
+                "custom_server" -> {
+                    stringResource(R.string.Server_Choice_Dialog_AddCustom)
+                }
+
+                else -> getServerName(it)
+            },
+            modifier = Modifier
+                .padding(vertical = 12.dp)
+                .fillMaxWidth()
+        )
+    }
+}

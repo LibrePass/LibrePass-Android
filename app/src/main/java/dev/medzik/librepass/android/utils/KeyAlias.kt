224721d856726f9c4d7240f2ce822f27da45f9ce
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-05-30 21:17:46 +0200

    Move secrets and app version datastore to database-logic module

--- app/src/main/java/dev/medzik/librepass/android/utils/KeyAlias.kt
+++ app/src/main/java/dev/medzik/librepass/android/utils/KeyAlias.kt
@@ -3,6 +3,5 @@ package dev.medzik.librepass.android.utils
 import dev.medzik.android.crypto.KeyStoreAlias
 
 enum class KeyAlias : KeyStoreAlias {
-    BiometricAesKey,
-    DataStoreEncrypted
+    BiometricAesKey
 }

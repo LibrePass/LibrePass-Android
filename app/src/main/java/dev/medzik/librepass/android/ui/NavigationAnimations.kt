aa729d5999e39eaf7d8eb3b5feca420d95ac5301
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-06-08 11:51:46 +0200

    Update navigation animations

---
+++ app/src/main/java/dev/medzik/librepass/android/ui/NavigationAnimations.kt
@@ -0,0 +1,57 @@
+package dev.medzik.librepass.android.ui
+
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+
+object NavigationAnimations {
+    private const val TRANSMISSION_DURATION_MILLS = 400
+
+    private fun <T> tweenEasing(
+        durationMillis: Int
+    ) = tween<T>(
+        durationMillis = durationMillis,
+        easing = LinearOutSlowInEasing,
+    )
+
+    private fun <T> tweenFade(
+        durationMillis: Int
+    ) = tween<T>(
+        durationMillis = durationMillis,
+        easing = FastOutLinearInEasing,
+    )
+
+    fun enterTransition(): EnterTransition = slideInHorizontally(
+        animationSpec = tweenEasing(TRANSMISSION_DURATION_MILLS),
+        initialOffsetX = { fullWidth -> fullWidth / 12 },
+    ) + fadeIn(
+        animationSpec = tweenEasing(TRANSMISSION_DURATION_MILLS / 2),
+    )
+
+    fun exitTransition(): ExitTransition = slideOutHorizontally(
+        animationSpec = tweenFade(TRANSMISSION_DURATION_MILLS),
+        targetOffsetX = { fullWidth -> fullWidth / 8 },
+    ) + fadeOut(
+        animationSpec = tweenFade(TRANSMISSION_DURATION_MILLS / 2),
+    )
+
+    fun popEnterTransition(): EnterTransition = slideInHorizontally(
+        animationSpec = tweenEasing(TRANSMISSION_DURATION_MILLS),
+        initialOffsetX = { fullWidth -> -fullWidth / 12 },
+    ) + fadeIn(
+        animationSpec = tweenEasing(TRANSMISSION_DURATION_MILLS / 2),
+    )
+
+    fun popExitTransition(): ExitTransition = slideOutHorizontally(
+        animationSpec = tweenFade(TRANSMISSION_DURATION_MILLS),
+        targetOffsetX = { fullWidth -> fullWidth / 8 },
+    ) + fadeOut(
+        animationSpec = tweenFade(TRANSMISSION_DURATION_MILLS / 2),
+    )
+}

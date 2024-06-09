cd0e68ef73f76850c02be867fe65e582d9ba477d
Oskar Karpiński <me@medzik.dev>
2024-06-09 19:18:11 +0200

    Bump dev.medzik.android to 1.5.2

--- app/src/main/java/dev/medzik/librepass/android/ui/screens/settings/Settings.kt
+++ app/src/main/java/dev/medzik/librepass/android/ui/screens/settings/Settings.kt
@@ -4,11 +4,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Fingerprint
 import androidx.compose.material.icons.filled.ManageAccounts
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
-import dev.medzik.android.components.ui.PreferenceEntry
+import dev.medzik.android.components.ui.IconBox
+import dev.medzik.android.components.ui.preference.BasicPreference
 import dev.medzik.librepass.android.R
 import kotlinx.serialization.Serializable
 
@@ -18,14 +18,14 @@ object Settings
 @Composable
 fun SettingsScreen(navController: NavController) {
     Column {
-        PreferenceEntry(
-            icon = { Icon(Icons.Default.Fingerprint, contentDescription = null) },
+        BasicPreference(
             title = stringResource(R.string.Settings_Security),
+            leading = { IconBox(Icons.Default.Fingerprint) },
             onClick = { navController.navigate(SettingsSecurity) }
         )
 
-        PreferenceEntry(
-            icon = { Icon(Icons.Default.ManageAccounts, contentDescription = null) },
+        BasicPreference(
+            leading = { IconBox(Icons.Default.ManageAccounts) },
             title = stringResource(R.string.Settings_Account),
             onClick = { navController.navigate(SettingsAccount) }
         )
--- app/src/main/java/dev/medzik/librepass/android/ui/screens/settings/SettingsAccount.kt
+++ app/src/main/java/dev/medzik/librepass/android/ui/screens/settings/SettingsAccount.kt
@@ -5,13 +5,13 @@ import androidx.compose.material.icons.automirrored.filled.Logout
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.LockReset
 import androidx.compose.material.icons.filled.NoAccounts
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
-import dev.medzik.android.components.ui.PreferenceEntry
+import dev.medzik.android.components.ui.IconBox
+import dev.medzik.android.components.ui.preference.BasicPreference
 import dev.medzik.librepass.android.R
 import dev.medzik.librepass.android.ui.LibrePassViewModel
 import dev.medzik.librepass.android.ui.screens.Welcome
@@ -29,21 +29,21 @@ fun SettingsAccountScreen(
     navController: NavController,
     viewModel: LibrePassViewModel = hiltViewModel()
 ) {
-    PreferenceEntry(
+    BasicPreference(
         title = stringResource(R.string.ChangeEmail),
-        icon = { Icon(Icons.Default.Email, contentDescription = null) },
+        leading = { IconBox(Icons.Default.Email) },
         onClick = { navController.navigate(SettingsAccountChangeEmail) }
     )
 
-    PreferenceEntry(
+    BasicPreference(
         title = stringResource(R.string.ChangePassword),
-        icon = { Icon(Icons.Default.LockReset, contentDescription = null) },
+        leading = { IconBox(Icons.Default.LockReset) },
         onClick = { navController.navigate(SettingsAccountChangePassword) }
     )
 
-    PreferenceEntry(
+    BasicPreference(
         title = stringResource(R.string.Logout),
-        icon = { Icon(Icons.AutoMirrored.Filled.Logout, contentDescription = null) },
+        leading = { IconBox(Icons.AutoMirrored.Filled.Logout) },
         onClick = {
             runBlocking {
                 val credentials = viewModel.credentialRepository.get()!!
@@ -63,9 +63,9 @@ fun SettingsAccountScreen(
         }
     )
 
-    PreferenceEntry(
+    BasicPreference(
         title = stringResource(R.string.DeleteAccount),
-        icon = { Icon(Icons.Default.NoAccounts, contentDescription = null) },
+        leading = { IconBox(Icons.Default.NoAccounts) },
         onClick = { navController.navigate(SettingsAccountDeleteAccount) }
     )
 }

bc90b368aab907e92cf32a87219f6968ff654e64
Oskar Karpiński <me@medzik.dev>
2024-07-03 18:04:49 +0200

    Rename dagger module object class `RoomModule` to `DatabaseProvider`

--- database-logic/src/main/java/dev/medzik/librepass/android/database/injection/DatabaseProvider.kt
+++ database-logic/src/main/java/dev/medzik/librepass/android/database/injection/DatabaseProvider.kt
@@ -13,7 +13,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RoomModule {
+object DatabaseProvider {
     @Singleton
     @Provides
     fun providesRepository(

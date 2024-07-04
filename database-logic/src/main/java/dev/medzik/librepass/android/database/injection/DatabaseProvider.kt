f04c3521e5c133e8893eca36df67a2a766fa7b88
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-07-04 16:27:32 +0200

    Rename `providesRepository` to `provideRepository` in dagger module named `DatabaseProvider`

--- database-logic/src/main/java/dev/medzik/librepass/android/database/injection/DatabaseProvider.kt
+++ database-logic/src/main/java/dev/medzik/librepass/android/database/injection/DatabaseProvider.kt
@@ -14,7 +14,7 @@ import javax.inject.Singleton
 object DatabaseProvider {
     @Singleton
     @Provides
-    fun providesRepository(@ApplicationContext context: Context): Repository {
+    fun provideRepository(@ApplicationContext context: Context): Repository {
         return Repository(context)
     }
 }

19a8926984c3ca81ece4c18e37a6b639684b839f
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-07-15 10:40:48 +0200

    Fix signup screen

--- ui-logic/src/main/java/dev/medzik/librepass/android/ui/auth/SignupViewModel.kt
+++ ui-logic/src/main/java/dev/medzik/librepass/android/ui/auth/SignupViewModel.kt
@@ -4,28 +4,36 @@ import android.content.Context
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dev.medzik.android.utils.showToast
 import dev.medzik.librepass.android.common.haveNetworkConnection
 import dev.medzik.librepass.android.ui.R
 import dev.medzik.librepass.client.Server
 import dev.medzik.librepass.client.api.AuthClient
+import javax.inject.Inject
 
-class SignupViewModel : ViewModel() {
+@HiltViewModel
+class SignupViewModel @Inject constructor(
+    @ApplicationContext private val context: Context
+) : ViewModel() {
     val email = mutableStateOf("")
     val password = mutableStateOf("")
     val retypePassword = mutableStateOf("")
-    val retypePasswordIsValid = password.value.isNotEmpty() &&
-            retypePassword.value == password.value
     val passwordHint = mutableStateOf("")
-    val canLogin = email.value.isNotEmpty() &&
-            password.value.isNotEmpty() &&
-            retypePassword.value == password.value
+
+    val retypePasswordError: Boolean
+        get() = retypePassword.value != password.value
+    val canSignup: Boolean
+        get() = email.value.isNotEmpty() &&
+                password.value.isNotEmpty() &&
+                !retypePasswordError
 
     val server = mutableStateOf(Server.PRODUCTION)
 
     private val authClient = AuthClient(apiUrl = server.value)
 
-    fun register(context: Context, navController: NavController) {
+    fun register(navController: NavController) {
         if (!context.haveNetworkConnection()) {
             context.showToast(R.string.NoInternetConnection)
             return
@@ -40,7 +48,7 @@ class SignupViewModel : ViewModel() {
             // TODO: disable back
             navController.navigate(Login)
         } catch (e: Exception) {
-
+            // TODO
         }
     }
 }

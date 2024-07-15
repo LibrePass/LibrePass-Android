07896de63656ca3c8d244f078aa34698419cfeb9
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-07-15 10:41:27 +0200

    Add dagger injection into `LoginViewModel`

--- ui-logic/src/main/java/dev/medzik/librepass/android/ui/auth/LoginScreen.kt
+++ ui-logic/src/main/java/dev/medzik/librepass/android/ui/auth/LoginScreen.kt
@@ -2,8 +2,6 @@ package dev.medzik.librepass.android.ui.auth
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,7 +14,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -29,9 +26,9 @@ import androidx.navigation.compose.rememberNavController
 import dev.medzik.android.compose.icons.TopAppBarBackIcon
 import dev.medzik.android.compose.ui.LoadingButton
 import dev.medzik.android.compose.ui.textfield.TextFieldValue
+import dev.medzik.librepass.android.business.injection.VaultCacheModule
+import dev.medzik.librepass.android.database.Repository
 import dev.medzik.librepass.android.ui.R
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -39,7 +36,10 @@ object Login
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen(navController: NavController) {
+fun LoginScreen(
+    navController: NavController,
+    viewModel: LoginViewModel = hiltViewModel()
+) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -52,80 +52,69 @@ fun LoginScreen(navController: NavController) {
             )
         }
     ) { innerPadding ->
-        LoginScreenContent(navController, innerPadding)
-    }
-}
-
-@Composable
-fun LoginScreenContent(
-    navController: NavController,
-    innerPadding: PaddingValues,
-    viewModel: LoginViewModel = hiltViewModel()
-) {
-    val context = LocalContext.current
-
-    LazyColumn(
-        modifier = Modifier
-            .padding(innerPadding)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        item {
-            Column(
-                modifier = Modifier.padding(horizontal = 12.dp),
-                verticalArrangement = Arrangement.spacedBy(12.dp)
-            ) {
-                EmailTextField(
-                    TextFieldValue.fromMutableState(
-                        state = viewModel.email
-                    )
-                )
-                PasswordTextField(
-                    TextFieldValue.fromMutableState(
-                        state = viewModel.password
-                    )
-                )
-
-                ChoiceServer(viewModel.server)
-            }
-        }
-
-        item {
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                LoadingButton(
-                    modifier = Modifier
-                        .padding(vertical = 12.dp)
-                        .height(50.dp)
-                        .fillMaxWidth(0.85f),
-                    onClick = { viewModel.login(context) },
-                    enabled = viewModel.email.value.isNotEmpty() && viewModel.email.value.isNotEmpty()
+        LazyColumn(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            item {
+                Column(
+                    modifier = Modifier.padding(horizontal = 12.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
-                    Text(
-                        text = stringResource(R.string.Login),
-                        style = MaterialTheme.typography.titleMedium
+                    EmailTextField(
+                        TextFieldValue.fromMutableState(
+                            state = viewModel.email
+                        )
                     )
-                }
+                    PasswordTextField(
+                        TextFieldValue.fromMutableState(
+                            state = viewModel.password
+                        )
+                    )
 
-                OutlinedButton(
-                    modifier = Modifier
-                        .padding(vertical = 12.dp)
-                        .height(45.dp)
-                        .fillMaxWidth(0.75f),
-                    onClick = {
-                        navController.navigate(
-                            ForgotPassword(
-                                server = viewModel.server.value
-                            )
+                    ChoiceServer(viewModel.server)
+                }
+            }
+
+            item {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    LoadingButton(
+                        modifier = Modifier
+                            .padding(vertical = 12.dp)
+                            .height(50.dp)
+                            .fillMaxWidth(0.85f),
+                        onClick = { viewModel.login() },
+                        enabled = viewModel.email.value.isNotEmpty() && viewModel.email.value.isNotEmpty()
+                    ) {
+                        Text(
+                            text = stringResource(R.string.Login),
+                            style = MaterialTheme.typography.titleMedium
                         )
                     }
-                ) {
-                    Text(
-                        text = stringResource(R.string.ForgotPassword),
-                        style = MaterialTheme.typography.titleSmall
-                    )
+
+                    OutlinedButton(
+                        modifier = Modifier
+                            .padding(vertical = 12.dp)
+                            .height(45.dp)
+                            .fillMaxWidth(0.75f),
+                        onClick = {
+                            navController.navigate(
+                                ForgotPassword(
+                                    server = viewModel.server.value
+                                )
+                            )
+                        }
+                    ) {
+                        Text(
+                            text = stringResource(R.string.ForgotPassword),
+                            style = MaterialTheme.typography.titleSmall
+                        )
+                    }
                 }
             }
         }
@@ -135,7 +124,17 @@ fun LoginScreenContent(
 @Preview
 @Composable
 fun LoginScreenPreview() {
+    val context = LocalContext.current
+    val repository = Repository(LocalContext.current)
+
     MaterialTheme {
-        LoginScreen(rememberNavController())
+        LoginScreen(
+            navController = rememberNavController(),
+            viewModel = LoginViewModel(
+                context = context,
+                repository = repository,
+                vaultCache = VaultCacheModule.provideVaultCache(repository)
+            )
+        )
     }
 }

bbf69f15f92a970285066c98c4e0bc4e685a2056
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-07-16 09:55:43 +0200

    Add view model to `ChoiceServer`

---
+++ ui-logic/src/main/java/dev/medzik/librepass/android/ui/auth/ChoiceServerViewModel.kt
@@ -0,0 +1,16 @@
+package dev.medzik.librepass.android.ui.auth
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.medzik.librepass.android.database.CustomServer
+import dev.medzik.librepass.android.database.Repository
+import javax.inject.Inject
+
+@HiltViewModel
+class ChoiceServerViewModel @Inject constructor(
+    private val repository: Repository
+) : ViewModel() {
+    fun getCustomServers() = repository.customServer.getAll()
+    suspend fun insertCustomServer(customServer: CustomServer) =
+        repository.customServer.insert(customServer)
+}

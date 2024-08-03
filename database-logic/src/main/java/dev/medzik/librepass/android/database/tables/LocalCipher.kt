4c8853972064b37533266c0d91215c9a31e6729f
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-08-03 16:06:44 +0200

    Move database tables to `tables` package

--- database-logic/src/main/java/dev/medzik/librepass/android/database/tables/LocalCipher.kt
+++ database-logic/src/main/java/dev/medzik/librepass/android/database/tables/LocalCipher.kt
@@ -1,9 +1,14 @@
-package dev.medzik.librepass.android.database
+package dev.medzik.librepass.android.database.tables
 
+import androidx.room.Dao
 import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.PrimaryKey
+import androidx.room.Query
 import androidx.room.TypeConverter
 import androidx.room.TypeConverters
+import androidx.room.Update
 import dev.medzik.librepass.client.utils.JsonUtils
 import dev.medzik.librepass.types.cipher.EncryptedCipher
 import java.util.*
@@ -38,4 +43,31 @@ class EncryptedCipherConverter {
     fun toEncryptedCipher(json: String): EncryptedCipher {
         return JsonUtils.deserialize(json)
     }
+}
+
+@Dao
+interface LocalCipherDao {
+    @Query("SELECT * FROM localCipher WHERE id = :id")
+    fun get(id: UUID): LocalCipher?
+
+    @Query("SELECT * FROM localCipher WHERE owner = :owner")
+    fun getAll(owner: UUID): List<LocalCipher>
+
+    @Query("SELECT id FROM localCipher WHERE owner = :owner")
+    fun getAllIDs(owner: UUID): List<UUID>
+
+    @Query("DELETE FROM localCipher WHERE id = :id")
+    fun delete(id: UUID)
+
+    @Query("DELETE FROM localCipher WHERE id IN (:ids)")
+    fun delete(ids: List<UUID>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(cipherTable: LocalCipher)
+
+    @Update
+    fun update(cipherTable: LocalCipher)
+
+    @Query("DELETE FROM localCipher WHERE owner = :owner")
+    suspend fun drop(owner: UUID)
 }

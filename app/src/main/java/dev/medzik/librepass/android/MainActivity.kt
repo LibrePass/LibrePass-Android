1880e393f767fe69ca30ace908c78d571006fc3e
Oskar Karpiński <me@medzik.dev>
2024-10-19 23:00:27 +0200

    Add deprecation warning

--- app/src/main/java/dev/medzik/librepass/android/MainActivity.kt
+++ app/src/main/java/dev/medzik/librepass/android/MainActivity.kt
@@ -5,10 +5,18 @@ import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.medzik.android.utils.openEmailApplication
+import dev.medzik.android.utils.showToast
 import dev.medzik.librepass.android.business.VaultCache
 import dev.medzik.librepass.android.common.popUpToDestination
 import dev.medzik.librepass.android.database.Repository
@@ -48,11 +56,37 @@ class MainActivity : FragmentActivity() {
         // retrieves aes key for vault decryption if key is valid
         vault.getSecretsIfNotExpired(this)
 
+        this.showToast("This application is deprecated.")
+
         setContent {
             LibrePassTheme(
                 darkTheme = isSystemInDarkTheme(),
                 dynamicColor = true
             ) {
+                var showDeprecatedWarning by remember { mutableStateOf(true) }
+                if (showDeprecatedWarning) {
+                    AlertDialog(
+                        onDismissRequest = {
+                            showDeprecatedWarning = false
+                        },
+                        confirmButton = {
+                            TextButton(
+                                onClick = {
+                                    showDeprecatedWarning = false
+                                }
+                            ) {
+                                Text("OK")
+                            }
+                        },
+                        title = {
+                            Text("Warning")
+                        },
+                        text = {
+                            Text("This application is deprecated. Please migrate all passwords before December 2024")
+                        }
+                    )
+                }
+
                 LibrePassNavigation()
             }
         }

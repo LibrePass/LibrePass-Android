97e3f39876c4d283395129db90c6d0ae39fa6a1a
Oskar Karpiński <karpinski.oskar@outlook.com>
2024-10-19 18:05:50 +0200

    Revert "Rename dagger module object class `RoomModule` to `DatabaseProvider`"
    This reverts commit bc90b368aab907e92cf32a87219f6968ff654e64.

--- database-logic/src/main/java/dev/medzik/librepass/android/database/injection/RoomModule.kt
+++ database-logic/src/main/java/dev/medzik/librepass/android/database/injection/RoomModule.kt
@@ -13,7 +13,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DatabaseProvider {
+object RoomModule {
     @Singleton
     @Provides
     fun providesRepository(

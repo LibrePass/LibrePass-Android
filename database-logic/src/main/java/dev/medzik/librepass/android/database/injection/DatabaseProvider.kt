84fb0dff583c0a7bd23355ca5adedf490c783f48
Oskar Karpiński <me@medzik.dev>
2024-10-19 18:05:44 +0200

    Revert "Delete `LibrePassViewModel` and in `DatabaseProvider` delete providers for Daos"
    This reverts commit 06df3882962698b90f2cc921f1a425c2cf45f186.

--- database-logic/src/main/java/dev/medzik/librepass/android/database/injection/DatabaseProvider.kt
+++ database-logic/src/main/java/dev/medzik/librepass/android/database/injection/DatabaseProvider.kt
@@ -6,6 +6,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import dev.medzik.librepass.android.database.CredentialsDao
+import dev.medzik.librepass.android.database.LocalCipherDao
 import dev.medzik.librepass.android.database.Repository
 import javax.inject.Singleton
 
@@ -14,7 +16,21 @@ import javax.inject.Singleton
 object DatabaseProvider {
     @Singleton
     @Provides
-    fun providesRepository(@ApplicationContext context: Context): Repository {
+    fun providesRepository(
+        @ApplicationContext context: Context
+    ): Repository {
         return Repository(context)
+    }
+
+    @Singleton
+    @Provides
+    fun providesCipherRepository(repository: Repository): LocalCipherDao {
+        return repository.cipher
+    }
+
+    @Singleton
+    @Provides
+    fun provideCredentialRepository(repository: Repository): CredentialsDao {
+        return repository.credentials
     }
 }

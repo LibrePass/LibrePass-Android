90c8597dd13870016dbc01a5f2122d12ce7467ee
Oskar Karpiński <me@medzik.dev>
2024-10-19 18:05:18 +0200

    Revert "Fix signup screen"
    This reverts commit 24f230632380f38c41eae7815bd6b1a880a8a98f.

--- ui-logic/src/main/java/dev/medzik/librepass/android/ui/auth/SignupViewModel.kt
+++ ui-logic/src/main/java/dev/medzik/librepass/android/ui/auth/SignupViewModel.kt
@@ -4,36 +4,28 @@ import android.content.Context
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
-import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dev.medzik.android.utils.showToast
 import dev.medzik.librepass.android.common.haveNetworkConnection
 import dev.medzik.librepass.android.ui.R
 import dev.medzik.librepass.client.Server
 import dev.medzik.librepass.client.api.AuthClient
-import javax.inject.Inject
 
-@HiltViewModel
-class SignupViewModel @Inject constructor(
-    @ApplicationContext private val context: Context
-) : ViewModel() {
+class SignupViewModel : ViewModel() {
     val email = mutableStateOf("")
     val password = mutableStateOf("")
     val retypePassword = mutableStateOf("")
+    val retypePasswordIsValid = password.value.isNotEmpty() &&
+            retypePassword.value == password.value
     val passwordHint = mutableStateOf("")
-
-    val retypePasswordError: Boolean
-        get() = retypePassword.value != password.value
-    val canSignup: Boolean
-        get() = email.value.isNotEmpty() &&
-                password.value.isNotEmpty() &&
-                !retypePasswordError
+    val canLogin = email.value.isNotEmpty() &&
+            password.value.isNotEmpty() &&
+            retypePassword.value == password.value
 
     val server = mutableStateOf(Server.PRODUCTION)
 
     private val authClient = AuthClient(apiUrl = server.value)
 
-    fun register(navController: NavController) {
+    fun register(context: Context, navController: NavController) {
         if (!context.haveNetworkConnection()) {
             context.showToast(R.string.NoInternetConnection)
             return
@@ -48,7 +40,7 @@ class SignupViewModel @Inject constructor(
             // TODO: disable back
             navController.navigate(Login)
         } catch (e: Exception) {
-            // TODO
+
         }
     }
 }

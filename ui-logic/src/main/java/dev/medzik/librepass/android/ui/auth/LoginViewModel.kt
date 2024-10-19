9fb03edd4737f28f55eada0ad47f65b99fdc84c7
Oskar Karpiński <me@medzik.dev>
2024-10-19 18:05:13 +0200

    Revert "Add dagger injection into `LoginViewModel`"
    This reverts commit 041bb68b6c70ead4a1a030ab8463c91674c895cf.

--- ui-logic/src/main/java/dev/medzik/librepass/android/ui/auth/LoginViewModel.kt
+++ ui-logic/src/main/java/dev/medzik/librepass/android/ui/auth/LoginViewModel.kt
@@ -3,32 +3,24 @@ package dev.medzik.librepass.android.ui.auth
 import android.content.Context
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dev.medzik.android.utils.showToast
-import dev.medzik.librepass.android.business.VaultCache
+import dev.medzik.librepass.android.business.injection.VaultCacheModule
 import dev.medzik.librepass.android.common.haveNetworkConnection
 import dev.medzik.librepass.android.database.Credentials
-import dev.medzik.librepass.android.database.Repository
+import dev.medzik.librepass.android.database.injection.DatabaseProvider
 import dev.medzik.librepass.android.ui.R
 import dev.medzik.librepass.client.Server
 import dev.medzik.librepass.client.api.AuthClient
 import dev.medzik.librepass.utils.fromHex
-import javax.inject.Inject
 
-@HiltViewModel
-class LoginViewModel @Inject constructor(
-    @ApplicationContext private val context: Context,
-    private val repository: Repository,
-    private val vaultCache: VaultCache
-) : ViewModel() {
+class LoginViewModel : ViewModel() {
     var email = mutableStateOf("")
     var password = mutableStateOf("")
     var server = mutableStateOf(Server.PRODUCTION)
 
     private val authClient = AuthClient(apiUrl = server.value)
 
-    suspend fun login() {
+    suspend fun login(context: Context) {
         if (!context.haveNetworkConnection()) {
             context.showToast(R.string.NoInternetConnection)
             return
@@ -40,6 +32,9 @@ class LoginViewModel @Inject constructor(
                 email = email.value,
                 password = password.value
             )
+
+            val repository = DatabaseProvider.provideRepository(context)
+            val vaultCache = VaultCacheModule.provideVaultCache(repository)
 
             val credentialsDbEntry = Credentials(
                 userId = credentials.userId,

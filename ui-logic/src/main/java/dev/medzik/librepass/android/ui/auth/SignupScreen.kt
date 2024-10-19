820a9213a9cc594d998f6b3cd1188a764b4dc4d1
Oskar Karpiński <me@medzik.dev>
2024-10-19 18:05:18 +0200

    Revert "Fix signup screen"
    This reverts commit 24f230632380f38c41eae7815bd6b1a880a8a98f.

--- ui-logic/src/main/java/dev/medzik/librepass/android/ui/auth/SignupScreen.kt
+++ ui-logic/src/main/java/dev/medzik/librepass/android/ui/auth/SignupScreen.kt
@@ -2,20 +2,26 @@ package dev.medzik.librepass.android.ui.auth
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.InputTransformation.Companion.keyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Password
 import androidx.compose.material.icons.filled.QuestionMark
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -34,6 +40,8 @@ import dev.medzik.android.compose.ui.textfield.AnimatedTextField
 import dev.medzik.android.compose.ui.textfield.PasswordAnimatedTextField
 import dev.medzik.android.compose.ui.textfield.TextFieldValue
 import dev.medzik.librepass.android.ui.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -41,12 +49,7 @@ object Signup
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SignupScreen(
-    navController: NavController,
-    viewModel: SignupViewModel = hiltViewModel()
-) {
-    val context = LocalContext.current
-
+fun SignupScreen(navController: NavController) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -59,87 +62,98 @@ fun SignupScreen(
             )
         }
     ) { innerPadding ->
-        LazyColumn(
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            item {
-                Column(
-                    modifier = Modifier.regularHorizontalPadding(),
-                    verticalArrangement = Arrangement.spacedBy(12.dp)
-                ) {
-                    EmailTextField(
-                        value = TextFieldValue.fromMutableState(
-                            state = viewModel.email
-                        )
-                    )
+        SignupScreenContent(navController, innerPadding)
+    }
+}
 
-                    PasswordAnimatedTextField(
-                        label = stringResource(R.string.Password),
-                        value = TextFieldValue.fromMutableState(
-                            state = viewModel.password,
-                            valueLabel = TextFieldValue.ValueLabel(
-                                type = TextFieldValue.ValueLabel.Type.WARNING,
-                                text = context.getString(R.string.PasswordLabel_WontBeRecover)
-                            )
-                        ),
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Email
-                        )
-                    )
+@Composable
+fun SignupScreenContent(
+    navController: NavController,
+    innerPadding: PaddingValues,
+    viewModel: SignupViewModel = hiltViewModel()
+) {
+    val context = LocalContext.current
 
-                    PasswordAnimatedTextField(
-                        label = stringResource(R.string.RetypePassword),
-                        value = TextFieldValue.fromMutableState(
-                            state = viewModel.retypePassword,
-                            error = if (viewModel.password.value.isNotEmpty() && viewModel.retypePasswordError) {
-                                "Passwords mismatch"
-                            } else null
-                        ),
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Email
-                        )
+    LazyColumn(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        item {
+            Column(
+                modifier = Modifier.regularHorizontalPadding(),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                EmailTextField(
+                    value = TextFieldValue.fromMutableState(
+                        state = viewModel.email
                     )
+                )
 
-                    AnimatedTextField(
-                        label = stringResource(R.string.PasswordHint),
-                        value = TextFieldValue.fromMutableState(
-                            state = viewModel.passwordHint,
-                            valueLabel = TextFieldValue.ValueLabel(
-                                type = TextFieldValue.ValueLabel.Type.INFO,
-                                text = context.getString(R.string.PasswordHintLabel)
-                            )
-                        ),
-                        leading = { IconBox(Icons.Default.QuestionMark) },
-                        keyboardOptions = KeyboardOptions(
-                            keyboardType = KeyboardType.Email
+                PasswordAnimatedTextField(
+                    label = stringResource(R.string.Password),
+                    value = TextFieldValue.fromMutableState(
+                        state = viewModel.password,
+                        valueLabel = TextFieldValue.ValueLabel(
+                            type = TextFieldValue.ValueLabel.Type.WARNING,
+                            text = context.getString(R.string.PasswordLabel_WontBeRecover)
                         )
+                    ),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email
                     )
+                )
 
-                    ChoiceServer(viewModel.server)
-                }
+                PasswordAnimatedTextField(
+                    label = stringResource(R.string.RetypePassword),
+                    value = TextFieldValue.fromMutableState(
+                        state = viewModel.retypePassword,
+                        error = if (!viewModel.retypePasswordIsValid) {
+                            "Passwords mismatch"
+                        } else null
+                    ),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email
+                    )
+                )
+
+                AnimatedTextField(
+                    label = stringResource(R.string.PasswordHint),
+                    value = TextFieldValue.fromMutableState(
+                        state = viewModel.passwordHint,
+                        valueLabel = TextFieldValue.ValueLabel(
+                            type = TextFieldValue.ValueLabel.Type.INFO,
+                            text = context.getString(R.string.PasswordHintLabel)
+                        )
+                    ),
+                    leading = { IconBox(Icons.Default.QuestionMark) },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email
+                    )
+                )
+
+                ChoiceServer(viewModel.server)
             }
+        }
 
-            item {
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally
+        item {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                LoadingButton(
+                    modifier = Modifier
+                        .padding(vertical = 12.dp)
+                        .height(50.dp)
+                        .fillMaxWidth(0.85f),
+                    onClick = { viewModel.register(context, navController) },
+                    enabled = viewModel.canLogin
                 ) {
-                    LoadingButton(
-                        modifier = Modifier
-                            .padding(vertical = 12.dp)
-                            .height(50.dp)
-                            .fillMaxWidth(0.85f),
-                        onClick = { viewModel.register(navController) },
-                        enabled = viewModel.canSignup
-                    ) {
-                        Text(
-                            text = stringResource(R.string.Signup),
-                            style = MaterialTheme.typography.titleMedium
-                        )
-                    }
+                    Text(
+                        text = stringResource(R.string.Signup),
+                        style = MaterialTheme.typography.titleMedium
+                    )
                 }
             }
         }
@@ -150,9 +164,6 @@ fun SignupScreen(
 @Composable
 fun SignupPreview() {
     MaterialTheme {
-        SignupScreen(
-            navController = rememberNavController(),
-            viewModel = SignupViewModel(context = LocalContext.current)
-        )
+        SignupScreen(rememberNavController())
     }
 }
